207c9a538d98a511e6362f60201d86c594ec99ae
FranciscoVeloz <franciscoveloz245@gmail.com>
2023-01-11 10:25:36 -0600

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import './globals.css'
+import React from 'react'
 
-export default function RootLayout({
-  children,
+export default function RootLayout ({
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html>
+      <head>
+        <title>Yard sale</title>
+        <meta content='width=device-width, initial-scale=1' name='viewport' />
+        <link rel='icon' href='/favicon.ico' />
+      </head>
       <body>{children}</body>
     </html>
   )
